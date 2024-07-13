57caae578d95fc5e9ab130611269f5dacdf64194
Smit Keni <smitkeni45@gmail.com>
2024-07-14 00:14:41 +0530

    final final final

--- lib/core/consts/constants.dart
+++
@@ -1,4 +0,0 @@
-class appConstants{
-  static const supabaseUrl = "https://bcvdcsuiebgvtrnyjlex.supabase.co";
-  static const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjdmRjc3VpZWJndnRybnlqbGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk3MzA3MjYsImV4cCI6MjAzNTMwNjcyNn0.AupWplMTXLZAaVZIgi49uPDwaoWGgWupuHWcvR-WmOo";
-}
